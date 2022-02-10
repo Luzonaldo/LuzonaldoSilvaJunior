aff905048975459d7fcc7217b7c508ef5e2c6096
luzo <luzo@luzo-Lenovo-G40-70>
2022-02-09 23:08:19 -0300

    1º commit

---
+++ praticaGit/src/praticaGit/HelloWorld.java
@@ -0,0 +1,12 @@
+package praticaGit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Primeira mensagem prática Git");
+		System.out.println(".................");
+		System.out.println("1ª modificação");
+
+	}
+
+}
