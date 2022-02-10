14ddfebc09524ae6d366acfbf7e69f9e8e32b552
luzo <luzo@luzo-Lenovo-G40-70>
2022-02-09 23:57:16 -0300

    2º commit

--- praticaGit/src/praticaGit/HelloWorld.java
+++ praticaGit/src/praticaGit/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Primeira mensagem prática Git");
 		System.out.println(".................");
 		System.out.println("1ª modificação");
+		System.out.println("2ª modificação");
 
 	}
 
